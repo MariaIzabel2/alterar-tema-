e571af008769a5efdeef7247040ace570ceafdc1
MariaIzabel2 <maria.izabel.silva27@escola.pr.gov.br>
2026-08-12 12:29:38 -0300

    12 de agosto

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ button{
     cursor: pointer;
 }
 
-const botao = document.getElementByld("tema");
+const botao = document.getElementById("tema");
 
 botao.addEventListener("click",function () {
     document.body.classList.toggle("dark");
